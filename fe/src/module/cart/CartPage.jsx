44cae2d9f9218bb6f1d5113aad450e6fd0ad4767
nvh2312 <mdr3132003@gmail.com>
2023-05-20 20:56:33 +0700

    update

--- fe/src/module/cart/CartPage.jsx
+++ fe/src/module/cart/CartPage.jsx
@@ -89,7 +89,7 @@ const CartPage = () => {
             </div>
 
             <div className="cart">
-              <div className="information-cart mt-7 bg-white text-base rounded-lg max-w-[90vw] sm:w-full overflow-x-auto">
+              <div className="information-cart mt-7 bg-white text-base rounded-lg max-w-[100vw] sm:w-full overflow-x-auto">
                 <Table>
                   <thead>
                     <tr>
