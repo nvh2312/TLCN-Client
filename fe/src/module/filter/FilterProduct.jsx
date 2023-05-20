6b3ee1dd9b2990ae5bec7e205911b6884ad35d52
nvh2312 <mdr3132003@gmail.com>
2023-05-20 20:32:37 +0700

    update

--- fe/src/module/filter/FilterProduct.jsx
+++ fe/src/module/filter/FilterProduct.jsx
@@ -77,7 +77,7 @@ const FilterProduct = ({ data }) => {
             }}
             bodyClassName="w-[430px] sm:w-[600px] md:w-[700px] lg:w-[1050px] bg-white p-10 rounded-lg relative z-10 content h-[500px] overflow-y-auto overflow-x-hidden"
           >
-            <table className="table-product items-center table-fixed w-full">
+            <table className="table-product items-center table-fixed w-full max-w-full">
               <thead>
                 <tr>
                   <th></th>
