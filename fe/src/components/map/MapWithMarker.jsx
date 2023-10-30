79bdfcf0a60e1e5317fef24994d722c1f9e54728
nvh2312 <mdr3132003@gmail.com>
2023-10-30 23:34:46 +0700

    fix icon location

--- fe/src/components/map/MapWithMarker.jsx
+++ fe/src/components/map/MapWithMarker.jsx
@@ -1,14 +1,23 @@
-import React from 'react';
-import 'leaflet/dist/leaflet.css';
-import { MapContainer, TileLayer, Marker } from 'react-leaflet';
+import React from "react";
+import "leaflet/dist/leaflet.css";
+import { MapContainer, TileLayer, Marker } from "react-leaflet";
 
 const MapWithMarker = ({ address }) => {
   const position = [address.latitude, address.longitude];
 
   return (
-    <MapContainer className='h-[500px] md:h-[600px] lg:h-[700px]' center={position} zoom={18}>
+    <MapContainer
+      className="h-[500px] md:h-[600px] lg:h-[700px]"
+      center={position}
+      zoom={18}
+    >
       <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-      <Marker position={position} />
+      <Marker
+        position={position}
+        icon={L.icon({
+          iconUrl: "/node_modules/leaflet/dist/images/marker-icon.png",
+        })}
+      />
     </MapContainer>
   );
 };
